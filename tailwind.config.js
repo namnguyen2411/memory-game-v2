d709167fabad7f0dce74b40d7d2e7872189f09cb
NPN-PC <phuongnam.nguyen241196@gmail.com>
2024-12-13 14:28:00 +0700

    style: add wiggle animation and update h1 styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ export default {
         red600: '#dc2626',
         cyan400: '#22d3ee'
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-20deg)' },
+          '50%': { transform: 'rotate(20deg)' }
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite'
+      }
     }
   },
   plugins: []
