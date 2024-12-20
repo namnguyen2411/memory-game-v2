9a14090d77dd9cf22a5dcc644d1d72643c3c1a1f
NPN-PC <phuongnam.nguyen241196@gmail.com>
2024-12-20 11:42:53 +0700

    feat: add streak state to store and update score state structure
    - Added new state: streak, to reward consecutive matching pairs.
    
    - Updated score state to an object with 2 properties:
    
      - current: the current score value.
    
      - matchingPair: an array of matched pair indices.

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,8 +7,10 @@ interface GameState {
   setGameStarted: () => void
   stage: number
   setStage: () => void
-  score: number
-  setScore: (stage: number) => void
+  streak: number
+  setStreak: (newStreak: number) => void
+  score: { current: number; matchingPair: number[][] }
+  setScore: (array: number[]) => void
   reset: () => void
 }
 
@@ -17,7 +19,16 @@ export const useStore = create<GameState>()((set) => ({
   setGameStarted: () => set((state) => ({ gameStarted: !state.gameStarted })),
   stage: 0,
   setStage: () => set((state) => ({ stage: state.stage + 1 })),
-  score: 0,
-  setScore: (stage: number) => set((state) => ({ score: state.score + POINTS_PER_MATCHING_PAIR * stage })),
-  reset: () => set(() => ({ stage: 0, score: 0, gameStarted: false }))
+  streak: 0,
+  setStreak: (newStreak) => set(() => ({ streak: newStreak })),
+  score: { current: 0, matchingPair: [] },
+  setScore: (array) =>
+    set((state) => ({
+      score: {
+        // +10 for each matching pair and +5 for every 2 streak
+        current: state.score.current + POINTS_PER_MATCHING_PAIR + (state.streak > 0 && state.streak % 2 === 0 ? 5 : 0),
+        matchingPair: [...state.score.matchingPair, array]
+      }
+    })),
+  reset: () => set(() => ({ stage: 0, streak: 0, score: { current: 0, matchingPair: [] }, gameStarted: false }))
 }))
